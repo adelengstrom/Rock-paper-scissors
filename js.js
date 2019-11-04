a968704305796bc0ab9d1d3337b0f60d3fa39409
bob <axelvilhelmtuomi7@gmail.com>
2019-11-04 13:46:24 +0200

    functons

--- js.js
+++ js.js
@@ -1,3 +1,36 @@
+//var userinput = null;
+
+
 function rock() {
-    window.prompt("hello")
+   
+    var computerinput = (Math.floor(Math.random()* 3))
+    if (computerinput == 0) {
+       alert("draw") 
+    } else if (computerinput ==1) {
+        alert("you lost")
+    } else {
+        alert("you win")
+    }
+}
+
+function paper() {
+  
+    var computerinput = (Math.floor(Math.random()* 3))
+    if (computerinput == 1) {
+       alert("draw") 
+    } else if (computerinput ==0) {
+        alert("you lost")
+    } else {
+        alert("you win")
+    }
+}
+function scissors() {
+    var computerinput = (Math.floor(Math.random()* 3))
+    if (computerinput == 2) {
+       alert("draw") 
+    } else if (computerinput == 1) {
+        alert("you lost")
+    } else {
+        alert("you win")
+    }
 }
