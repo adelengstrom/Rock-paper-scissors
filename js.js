5f5cd5f85227b6a5417c9254f028e157dbd65295
bob <axelvilhelmtuomi7@gmail.com>
2019-11-04 15:12:24 +0200

    made buttons work

--- js.js
+++ js.js
@@ -5,32 +5,58 @@ function rock() {
    
     var computerinput = (Math.floor(Math.random()* 3))
     if (computerinput == 0) {
-       alert("draw") 
+        var para = document.getElementById('winner');
+        para.innerText = 'Tie';
     } else if (computerinput == 1) {
-       document.getElementById("winner") = "computer";
+        var para = document.getElementById('winner');
+        para.innerText = 'You Win!';
     } else {
-        alert("you win")
+        var para = document.getElementById('winner');
+        para.innerText = 'You lose!';
     }
+    document.getElementById("rock").disabled = true;
+    document.getElementById("paper").disabled = true;
+    document.getElementById("scissors").disabled = true;
 }
 
 function paper() {
   
     var computerinput = (Math.floor(Math.random()* 3))
     if (computerinput == 1) {
-       alert("draw") 
+        var para = document.getElementById('winner');
+        para.innerText = 'Tie';
     } else if (computerinput ==0) {
-        alert("you lost")
+        var para = document.getElementById('winner');
+        para.innerText = 'You Win!';
     } else {
-        alert("you win")
+        var para = document.getElementById('winner');
+        para.innerText = 'You Lose!';
     }
+    document.getElementById("rock").disabled = true;
+    document.getElementById("paper").disabled = true;
+    document.getElementById("scissors").disabled = true;
 }
 function scissors() {
     var computerinput = (Math.floor(Math.random()* 3))
     if (computerinput == 2) {
-       alert("draw") 
+        var para = document.getElementById('winner');
+        para.innerText = 'Tie'; 
     } else if (computerinput == 1) {
-        alert("you lost")
+        var para = document.getElementById('winner');
+        para.innerText = 'You Win!';
     } else {
-        alert("you win")
+        var para = document.getElementById('winner');
+        para.innerText = 'You Lose!';
     }
+    document.getElementById("rock").disabled = true;
+    document.getElementById("paper").disabled = true;
+    document.getElementById("scissors").disabled = true;
+}
+function playagain() {
+    var para = document.getElementById('winner');
+    para.innerText = '';
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissors").disabled = false;
+
 }
